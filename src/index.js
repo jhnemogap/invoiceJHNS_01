f6d0fcdf581917dc6d1a301ccf4275bd3e8799b2
Jorge Humberto Nemogá Pinzón <50347390+jhnemogap@users.noreply.github.com>
2020-01-18 16:34:10 -0500

    Initial base structure

---
+++ src/index.js
@@ -0,0 +1,5 @@
+let $products = document.getElementById('products');
+
+let newProduct = document.createElement('div');
+newProduct.innerHTML = `<h2>Producto 1</h2>`;
+$products.appendChild(newProduct);
