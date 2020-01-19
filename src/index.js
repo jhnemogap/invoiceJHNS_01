fcb94d1af5ef5e4809c966df8c75cf35e58f793e
Jorge Humberto Nemogá Pinzón <50347390+jhnemogap@users.noreply.github.com>
2020-01-19 15:29:06 -0500

    v0.4.0
    * Test changes in repository from CodeSandBox
    
    * Header finalizado 99%
    
    * Update version number
    
    * Main section products HTML5 & CSS - test in JS
    
    * Main section products HTML5 & CSS - test in JS
    * Prueba corriendo el mismo sanbox - conflicto?
    * Finish main HTML and CSS
    * Update version 0.4.0 - no tag

--- src/index.js
+++ src/index.js
@@ -1,5 +1,66 @@
 let $products = document.getElementById('products');
+let $priceSubTotal = document.getElementById('price-subTotal');
+let $priceTotal = document.getElementById('price-total');
+const templateProduct = () => `
+<div class="products-attributes-noDisplay noPrint">
+  <h5>CANTIDAD</h5>
+  <h5>DESCRIPCIÓN</h5>
+  <h5>VR. UNIT.</h5>
+  <h5>VR. TOTAL</h5>
+  <h5>Eliminar</h5>
+</div>
 
-let newProduct = document.createElement('div');
-newProduct.innerHTML = `<h2>Producto 1</h2>`;
+<input
+  type="number"
+  value="0"
+  min="1"
+  class="product-attribute product-amount"
+/>
+
+<textarea
+  placeholder="X"
+  class="product-attribute product-descript"
+  wrap="hard"
+  cols="52"
+  rows="1"
+  spellcheck="true"
+></textarea>
+
+<div class="product-attribute product-vr-unit">
+  <span class="currency-symbol">$</span>
+  <input
+    type="number"
+    value="0"
+    min="1"
+    class="product-value"
+  />
+</div>
+
+<div class="product-attribute product-vr-total">
+  <span>$</span>
+  <input
+    type="number"
+    value="0"
+    min="1"
+    disabled
+    class="product-value"
+  />
+</div>
+
+<button class="product-attribute noPrint">del</button>
+`;
+
+let newProduct = document.createElement('article');
+newProduct.setAttribute('id', 'product-0');
+newProduct.classList.add('product');
+newProduct.innerHTML = templateProduct();
 $products.appendChild(newProduct);
+
+newProduct = document.createElement('article');
+newProduct.setAttribute('id', 'product-1');
+newProduct.classList.add('product');
+newProduct.innerHTML = templateProduct();
+$products.appendChild(newProduct);
+
+$priceSubTotal.append(`1560000`);
+$priceTotal.append(`1560000`);
