609f8b7d2b0f46f613fe71b2d7c94aeceab8d3f8
Jorge Humberto Nemogá Pinzón <50347390+jhnemogap@users.noreply.github.com>
2020-01-18 23:47:35 -0500

    Main section products HTML5 & CSS - test in JS3

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 let $products = document.getElementById('products');
 
-let newProduct = document.createElement('div');
-newProduct.innerHTML = `<h2>Producto 1</h2>`;
+let newProduct = document.createElement('article');
+newProduct.setAttribute('id', 'product-0');
+newProduct.classList.add('product');
+newProduct.innerHTML = `
+    <p class="product__amount">100</p>
+    <p class="product__descrip">El producto 1 con no se que cositas</p>
+    <p class="product__vr-unit"><span>$</span>15200</p>
+    <p class="product__vr-total"><span>$</span>1520000</p>
+    <p class="noPrint">S</p>
+  `;
+$products.appendChild(newProduct);
+
+newProduct = document.createElement('article');
+newProduct.setAttribute('id', 'product-1');
+newProduct.classList.add('product');
+newProduct.innerHTML = `
+    <p class="product__amount">8</p>
+    <p class="product__descrip">El producto 2 con no se que cositas xder</p>
+    <p class="product__vr-unit"><span>$</span>5000</p>
+    <p class="product__vr-total"><span>$</span>40000</p>
+    <p class="noPrint">N</p>
+  `;
 $products.appendChild(newProduct);
